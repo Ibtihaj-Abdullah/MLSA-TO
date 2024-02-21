acaecf50efea1d42e66a89bcbfd9ff54348f32a3
Ebtihaj <85163024+Ibtihaj-Abdullah@users.noreply.github.com>
2024-02-21 21:07:26 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Ebtihaj Abdullah",
+  title: "Engineer",
+  email: "ebtihajabdullah@acm.org",
+  gitHub: "https://github.com/Ibtihaj-Abdullah",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/ebtihaj-abdullah/",
   medium: "",
-  twitter: "microsoft",
+  twitter: "https://twitter.com/Ibtihaj_EA",
   youTube: "Code",
 };
 
