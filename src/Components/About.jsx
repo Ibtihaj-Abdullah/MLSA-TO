5e84bad94b4c1b5d202a3f8d5e28e5a470f7b1f4
Ebtihaj <85163024+Ibtihaj-Abdullah@users.noreply.github.com>
2024-02-21 21:08:39 +0500

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,15 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Systems Engineer studying at UET PEshawar.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Hardware Design",
+  "Software Design"
 ];
 
 /**
@@ -45,7 +41,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Eager to learn and explore new ideas, technologies and perspectives. As a passionate and driven professional, I am constantly seeking new opportunities to expand my knowledge and make a positive impact through my knowledge-seeking and innovative traits. My keen observation and ability to see things from different angles allows me to approach challenges with fresh insights and innovative solutions..";
 
 const About = () => {
   return (
