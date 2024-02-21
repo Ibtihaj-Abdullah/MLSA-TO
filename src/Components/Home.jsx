e2a056f3d8b3ba767146b35478531a183fce24fa
Ebtihaj <85163024+Ibtihaj-Abdullah@users.noreply.github.com>
2024-02-21 21:18:00 +0500

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/";
+import image from "../images/IMG.jpg";
 
 const imageAltText = "Ebtihaj";
 
